eaf4efc9258181859c8fdc6a2fd728849143757d
Kevin <yang.kevin.100@gmail.com>
2025-02-09 22:15:39 -0500

    add some css and write a small first post

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Something here",
+	title: "Name TBD",
 	url: "https://zorgonia.github.io",
 	language: "en",
 	description: "There's nothing here yet",
 	author: {
-		name: "Kyang",
-		email: "TBD",
+		name: "Zorgonia",
+		email: "personlol2013[at]gmail.com",
 		url: "https://zorgonia.github.io/about/"
 	}
 }
--- content/posts/ja/test-ja.md
+++
@@ -1,7 +0,0 @@
----
-title: テスト
-description: Testing
-date: 2025-02-08
-tags: not this one
----
-Testing 123
